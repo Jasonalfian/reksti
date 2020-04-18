c7156b951d639e26034b1086c8c1a03c94c34b3e
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2020-04-18 10:43:16 +0700

    Add model

--- smartcampus/admin.py
+++ smartcampus/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from .models import Course, Student, RoomConfiguration, AttendanceRecord
 # Register your models here.
+
+
+class CourseAdmin(admin.ModelAdmin):
+    fields = ['course_no', 'course_name']
+    list_display = ('course_no', 'course_name')
+
+
+class StudentAdmin(admin.ModelAdmin):
+    fields = ['student_id', 'name']
+    list_display = ('student_id', 'name')
+
+
+class RoomConfigurationAdmin(admin.ModelAdmin):
+    fields = ['room_no', 'course', 'class_no']
+    list_display = ('room_no', 'course', 'class_no', 'updated_at')
+
+
+class AttendanceRecordAdmin(admin.ModelAdmin):
+    fields = ['room_no', 'course', 'class_no', 'student', 'recorded_at']
+    list_display = ('room_no', 'course', 'class_no', 'student', 'recorded_at')
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(RoomConfiguration, RoomConfigurationAdmin)
+admin.site.register(AttendanceRecord, AttendanceRecordAdmin)
--- smartcampus/models.py
+++ smartcampus/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Course(models.Model):
+    course_no = models.CharField(max_length=10, primary_key=True)
+    course_name = models.TextField()
+
+    def __str__(self):
+        return f'{self.course_no} {self.course_name}'
+
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=8, primary_key=True)
+    name = models.TextField()
+
+    def __str__(self):
+        return f'{self.student_id} {self.name}'
+
+
+class RoomConfiguration(models.Model):
+    room_no = models.CharField(max_length=10, primary_key=True)
+    course = models.ForeignKey(Course, on_delete=models.PROTECT)
+    class_no = models.IntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.course} in {self.room_no}'
+
+
+class AttendanceRecord(models.Model):
+    room_no = models.CharField(max_length=10)
+    course = models.ForeignKey(Course, on_delete=models.PROTECT)
+    class_no = models.IntegerField()
+    student = models.ForeignKey(Student, on_delete=models.PROTECT)
+    recorded_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
